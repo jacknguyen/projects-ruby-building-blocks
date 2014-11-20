e6ed837890338caedd5affe89303b8454e26cfa6
Jack Nguyen <nguyen.jack@gmail.com>
2014-11-19 17:17:54 -0800

    project 1 caesar cipher complete

---
+++ caesar_cipher.rb
@@ -0,0 +1,27 @@
+def caesar_cipher string, num
+  words = string.split
+  # print words
+  words.map! do |word|
+    word.split('').map! do |letter|
+      if ((65..90).include? letter.ord) || ((97..122).include? letter.ord)
+        count = num
+        letter = letter.ord
+        while count > 0
+          letter += 1
+          if (letter == 91) || (letter == 123)
+            letter -= 26
+          end
+          count -= 1
+        end
+        letter.chr
+      else
+        letter = letter
+      end
+
+    end
+  end
+
+  words.map! { |word| word.join('') }
+
+  words = words.join(' ')
+end
